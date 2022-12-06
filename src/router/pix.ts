80c736ca1d49406fd0f24e87b67d5fc3dce51f72
Alexandre Henrique R Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2022-12-06 14:59:01 -0300

    base de recepcao

---
+++ src/router/pix.ts
@@ -0,0 +1,6 @@
+import express from 'express';
+
+export const PixRouter = express.Router();
+
+//pix
+//------------------------------------------------------------------------------
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import express, { json } from 'express';
 import cors from 'cors'
+import { PixRouter } from './router/pix';
 
 
 const app = express();
 app.use(json());
 app.use(cors());
+app.use(PixRouter);
 
 
-app.listen(process.env.PORT || 3055, async function () {
+app.listen(process.env.PORT || 3059, async function () {
  
   console.log('🚀🚀🤖 servidor em execução 🤖🚀🚀')
   console.log(`🚀🚀🤖 ${process.env.SERVE_CONSULT} 🤖🚀🚀`)
